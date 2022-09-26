e29dd8cd0109dbd36946e7a5c7145eafc0f0bda7
Dusan95z <dusan204z@gmail.com>
2022-09-26 16:57:48 +0200

    footer dark theme done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,12 @@
 import './_footer.scss';
+import { useContext } from 'react';
+import NavigationContext from '../../context/NavigationContext';
 
 const Footer = () => {
+  const { darkMode } = useContext(NavigationContext);
+
   return (
-    <footer>
+    <footer className={darkMode ? '' : 'lightTheme-footer'}>
       <p>&copy; Copyright 2022 Dušan Živanović</p>
       <p>All Rights Reserved</p>
     </footer>
