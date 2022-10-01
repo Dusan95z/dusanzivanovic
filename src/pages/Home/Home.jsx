1e13ebd8c49cf6d08a28cbe74faf9c1f2a2d0906
Dusan95z <dusan204z@gmail.com>
2022-10-02 00:18:50 +0200

    deleted calendar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,17 +2,18 @@ import './_home.scss';
 import { useContext } from 'react';
 import ThemeContext from '../../context/ThemeContext';
 import Clock from '../../features/Clock/Clock';
-import CalendarComponent from '../../features/Calendar/Calendar';
 
 const Home = () => {
   const { darkMode } = useContext(ThemeContext);
 
   return (
     <main className={darkMode ? 'mainHome' : 'mainHome lightTheme-mainHome'}>
-      <CalendarComponent />
       <Clock />
     </main>
   );
 };
 
 export default Home;
+
+// https://www.youtube.com/watch?v=o1yMqPyYeAo
+// https://github.com/lashaNoz/Calendar
