ba3760ee671c5b05a451c8c0a55387aa3146c5d3
Dusan95z <dusan204z@gmail.com>
2022-10-01 11:39:34 +0200

    calendar added

---
+++ src/features/Calendar/Calendar.jsx
@@ -0,0 +1,16 @@
+import 'react-calendar/dist/Calendar.css';
+import './_calendar.scss';
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+
+const CalendarComponent = () => {
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <div>
+      <Calendar onChange={onChange} value={value} />
+    </div>
+  );
+};
+
+export default CalendarComponent;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,15 @@
-import Clock from '../../features/Clock/Clock';
 import './_home.scss';
+import { useContext } from 'react';
+import ThemeContext from '../../context/ThemeContext';
+import Clock from '../../features/Clock/Clock';
+import CalendarComponent from '../../features/Calendar/Calendar';
 
 const Home = () => {
+  const { darkMode } = useContext(ThemeContext);
+
   return (
-    <main className='mainHome'>
+    <main className={darkMode ? 'mainHome' : 'mainHome lightTheme-mainHome'}>
+      <CalendarComponent />
       <Clock />
     </main>
   );
