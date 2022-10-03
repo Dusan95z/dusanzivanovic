aee10ba72345fbd688170d60a183472089fedb0c
Dusan95z <dusan204z@gmail.com>
2022-10-04 00:04:51 +0200

    react calendar again

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import './_home.scss';
 import { useContext } from 'react';
 import ThemeContext from '../../context/ThemeContext';
 import Clock from '../../features/Clock/Clock';
-import Calendar from '../../features/Calendar/Calendar';
+import ReactCalendar from '../../features/Calendar/Calendar';
 
 const Home = () => {
   const { darkMode } = useContext(ThemeContext);
@@ -10,12 +10,11 @@ const Home = () => {
   return (
     <main className={darkMode ? 'mainHome' : 'mainHome lightTheme-mainHome'}>
       <Clock />
-      <Calendar />
+      <ReactCalendar />
     </main>
   );
 };
 
 export default Home;
 
-// https://www.youtube.com/watch?v=m9OSBJaQTlM  js
-// https://www.youtube.com/watch?v=Q5Xen_Y7lUk  react
+// #167e56
