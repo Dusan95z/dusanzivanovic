229a971aacec9ecf612b6500f7ff439530d07d35
Dusan95z <dusan204z@gmail.com>
2022-10-04 12:22:37 +0200

    light mode calendar added

--- src/features/Calendar/Calendar.jsx
+++ src/features/Calendar/Calendar.jsx
@@ -1,9 +1,13 @@
 import 'react-calendar/dist/Calendar.css';
 import './_calendar.scss';
 import Calendar from 'react-calendar';
+import { useContext } from 'react';
+import ThemeContext from '../../context/ThemeContext';
 
 const ReactCalendar = () => {
-  return <Calendar />;
+  const { darkMode } = useContext(ThemeContext);
+
+  return <Calendar className={darkMode ? '' : 'lightTheme-calendar'} />;
 };
 
 export default ReactCalendar;
